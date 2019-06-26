6131b65daf9285359c4704a58c94f468d76007dc
zhoujie <2561535248@qq.com>
2019-06-26 16:46:08 +0800

    修改demo添加一只小猪

--- gitdemo/src/main/java/com/itcast/demo.java
+++ gitdemo/src/main/java/com/itcast/demo.java
@@ -2,6 +2,8 @@ package com.itcast;
 
 public class demo {
     public static void main(String[] args) {
-        System.out.println("你好世界");
+        System.out.println("你好世界");   
+		System.out.println("一只小猪");
+		
     }
 }
